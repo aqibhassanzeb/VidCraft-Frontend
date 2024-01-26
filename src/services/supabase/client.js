4c5a442d7091b5f3286f43ff292501504a0c31e3
aqib <aqibhassanzeb@gmail.com>
2024-01-26 23:38:29 +0500

    key added

--- src/services/supabase/client.js
+++ src/services/supabase/client.js
@@ -1,9 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Create a single supabase client for interacting with your database
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  supabaseUrl,
+  supabaseKey
 );
 
+console.log("supabase createClient :",supabase)
 export default supabase;
